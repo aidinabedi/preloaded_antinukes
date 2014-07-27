e181c9e6db9fc4590d576806521c4f4ce5fef1f5
Justin Love <git@JustinLove.name>
2014-07-27 13:46:50 -0500

    landing ping effect

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,6 +48,7 @@ module.exports = function(grunt) {
               'README.md',
               'CHANGELOG.md',
               'ui/**',
+              'pa/**',
               'server-script/**'],
             dest: modPath,
           },
